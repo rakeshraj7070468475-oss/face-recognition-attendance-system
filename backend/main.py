ed7ccd41d61ede0bb8fd377d3566bd5658a79b17
Rakesh Singh <rakeshraj7070468475@gmail.com>
2026-05-25 02:27:07 +0530

    Fixed production CORS

--- backend/main.py
+++ backend/main.py
@@ -41,9 +41,10 @@ app.add_middleware(
     CORSMiddleware,
 
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://facialrecognitionattendance.netlify.app",
+],
 
     allow_credentials=True,
 
